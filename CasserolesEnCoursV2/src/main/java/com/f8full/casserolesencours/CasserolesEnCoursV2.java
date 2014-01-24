80af4a10075b5ea6684a1acf8902b6a4f8049b15
F8Full <f8full@gmail.com>
2014-01-24 15:14:15 -0500

    - Added icons - Added copyright notice

--- CasserolesEnCoursV2/src/main/java/com/f8full/casserolesencours/CasserolesEnCoursV2.java
+++ CasserolesEnCoursV2/src/main/java/com/f8full/casserolesencours/CasserolesEnCoursV2.java
@@ -1,3 +1,31 @@
+/*------------------------------------------------------------------------------
+ **     Ident: Fabrice V.
+ **    Author: Fabrice V.
+ ** Copyright: (c) January 24, 2014 Fabrice Veniard All Rights Reserved.
+ **------------------------------------------------------------------------------
+ ** Fabrice V.                       |  No part of this file may be reproduced
+ ** @f8full                          |  or transmitted in any form or by any
+ **                                  |  means, electronic or mechanical, for the
+ ** H2J Montréal                     |  purpose, without the express written
+ ** Québec                           |  permission of the copyright holder.
+ *------------------------------------------------------------------------------
+ *
+ *   This file is part of casserolesencours.
+ *
+ *   casserolesencours is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   casserolesencours is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with casserolesencours.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 package com.f8full.casserolesencours;
 
 import java.util.Locale;
